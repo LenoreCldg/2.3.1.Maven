e9268daf19264fb1ed31fe028ccbc1c0fbf22ea5
Yulia Khrenenko <romanova_jv@mail.ru>
2020-07-03 16:39:52 +0300

    Initial commit

---
+++ src/test/java/BonusServiceTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateRegisteredUnderLimit() {
+
+        BonusService service = new BonusService();
+
+        long amount = 1000_00;
+        boolean registered = true;
+        long expected = 30;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateRegisteredOverLimit() {
+
+        BonusService service = new BonusService();
+
+        long amount = 1000_000_00;
+        boolean registered = true;
+        long expected = 500;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateUnregisteredUnderLimit() {
+
+        BonusService service = new BonusService();
+
+        long amount = 1000_00;
+        boolean registered = false;
+        long expected = 10;
+
+        long actual = service.calculate(amount, registered);
+
+        assertEquals(expected, actual);
+
+    }
+
+}
